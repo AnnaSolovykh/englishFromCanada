805deb796e5bd5a2f276c71882d8e476289621cf
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-05-12 20:44:32 +0300

    working sliders

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-
-//const slides = document.querySelectorAll(".slider-slide");
-//const dots = document.querySelectorAll(".dot");
-
-//let slideIndex = 1;
-
-//showSlides(slideIndex);
 let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
-
+let slideId = ["mySlides1", "mySlides2"];
 let dotsId = ["Dots1", "Dots2"]
 
 showSlides(1, 0);
@@ -17,77 +9,46 @@ function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
 }
 
-//  function currentSlide(n, no) {
-//    showSlides(slideIndex[no] = n);
-//  }
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+
 
 
 function showSlides(n,no) {
-
   let i;
+  var eachSlider = document.getElementsByClassName(slideId[no]);
+  let eachDot = document.getElementsByClassName(dotsId[no]);
+  if (n === undefined){n = ++slideIndex[no]};
+  if (n > eachSlider.length) {slideIndex[no] = 1};
+  if (n < 1) {slideIndex[no] = eachSlider.length};
 
-  let x = document.getElementsByClassName(slideId[no]);
-  // let y = document.getElementsByClassName(dotsId[no]);
-
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+  for (i = 0; i < eachSlider.length; i++) {
+    eachSlider[i].style.display = "none";  
   }
-  // for (i = 0; i < y.length; i++) {
-  //   y[i].className = y[i].className.replace(" active", "");
-  // }
-  x[slideIndex[no]-1].style.display = "block"; 
-  // y[slideIndex-1].className += " active";
-  //slides[slideIndex-1].style.display = "block";  
-  //x[slideIndex[no]-1].className += " active";
-  
+
+  for (i = 0; i < eachDot.length; i++) {
+    eachDot[i].className = eachDot[i].className.replace(" active", "");
+  }
+  eachSlider[slideIndex[no]-1].style.display = "block"; 
+  eachDot[slideIndex[no]-1].className += " active";
 }
 
-/*
+let intervalOne = setInterval(carousel, 3000, 1, 0);
+let intervalTwo = setInterval(carousel, 3000, 1, 1);
 
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  }
-
-
-let interval = setInterval( carousel,3000);
-
-function carousel(){
-   ;
-  slideIndex+=1; 
-  showSlides(slideIndex);
+function carousel(n, no){
+  n = 1
+  slideIndex[no] += n
+  showSlides(slideIndex[no], no);
 }
 
-document.querySelector(".phone-container").addEventListener("click", () => {
-  clearTimeout(interval);
-  interval = setInterval(carousel, 10000);
-  
+document.querySelector(".phone-container-one").addEventListener("click", () => {
+  clearTimeout(intervalOne);
+  intervalOne = setInterval(carousel, 10000, 1, 0);
 })
 
-
-let interval = setInterval(carousel,3000);
-
-function carousel(no){
-  let x = document.getElementsByClassName(slideId[no])
-  slideIndex[x]+=1; 
-  showSlides(slideIndex[x]);
-}
-
-
-let phoneContainer = document.querySelectorAll(".phone-container");
-phoneContainer.forEach(container => {
-  container.addEventListener("click", () => {
-    clearTimeout(interval);
-    interval = setInterval(carousel, 10000);
-  })
+document.querySelector(".phone-container-two").addEventListener("click", () => {
+  clearTimeout(intervalTwo);
+  intervalTwo = setInterval(carousel, 10000, 1, 1);
 })
-  */
