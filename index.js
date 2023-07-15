fc53f82db3031c268559ce546f1c171537b04bef
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-07-15 17:58:39 +0300

    links animation fixed

--- index.js
+++ index.js
@@ -7,19 +7,20 @@ burger.addEventListener('click', navSlide)
 
 function navSlide () {
   burger.classList.toggle('toggle');
-    nav.classList.toggle('nav-active');
-    navLinks.forEach((link, index)=> {
-        if (link.style.animation) {
-            link.style.animation = '';
-        }
-        else {
-            link.style.animation = `navLinkFade 0.1s ease forwards ${index / 7 + 1.2}s`;
-        }
-        link.addEventListener( 'click', function(){
-          nav.classList.remove('nav-active');
-          burger.classList.remove('toggle');
-        })
-    })
+  nav.classList.toggle('nav-active');
+  navLinks.forEach((link, index)=> {
+      // if (link.style.animation) {
+      //     link.style.animation = '';
+      // }
+      // else {
+      //     link.style.animation = `navLinkFade 0.1s ease forwards ${index / 7 + 1.2}s`;
+      // }
+      link.style.animation = `navLinkFade 0.1s ease forwards ${index / 7 + 1.2}s`;
+      link.addEventListener( 'click', function(){
+        nav.classList.remove('nav-active');
+        burger.classList.remove('toggle');
+      })
+  })
 }
 
 
