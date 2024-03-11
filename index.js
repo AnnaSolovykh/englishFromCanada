b36a7298b9df8b4893567b8e2a904f29f51c2ed7
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-03-10 22:35:48 -0400

    fix movile sliding nav animation

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function navSlide () {
   burger.classList.toggle('toggle');
   nav.classList.toggle('nav-active');
   navLinks.forEach((link, index)=> {
-      link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7 + 1.2}s`;
+      link.style.animation = `navLinkFade 0.4s ease forwards ${index / 7 + 0.2}s`;
       link.addEventListener( 'click', function(){
         nav.classList.remove('nav-active');
         burger.classList.remove('toggle');
